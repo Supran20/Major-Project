f29d6e6c09d5d2af5685e973144a2e2d6542e55c
sampada <sampadatandan127@gmail.com>
2025-01-19 10:02:15 +0545

    About page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -12,30 +12,58 @@ export const About = () => {
           <div className="container grid grid-two-cols">
             <div className="hero-content">
               {/* <p>We care to cure your Health</p> */}
-              
+
               <h1 class="heading-highlight">Why Choose Us?</h1>
-              <p>
-                Expertise: Our team consists of experienced IT professionals who
-                are passionate about staying up-to-date with the latest industry
-                trends.
-              </p>
-              <p>
-                Customization: We understand that every business is unique.
-                Thats why we create solutions that are tailored to your specific
-                needs and goals.
-              </p>
-              <p>
-                Customer-Centric Approach: We prioritize your satisfaction and
-                provide top-notch support to address your IT concerns.
-              </p>
-              <p>
-                Affordability: We offer competitive pricing without compromising
-                on the quality of our services.
-              </p>
-              <p>
-                Reliability: Count on us to be there when you need us. committed
-                to ensuring your IT environment is reliable and available 24/7.
-              </p>
+              <section class="expertise-section">
+                <div class="feature">
+                  <i class="icon fas fa-user-tie"></i>
+                  
+                  <h3>Expertise</h3>
+                  <p>
+                    Our team consists of experienced IT professionals who are
+                    passionate about staying up-to-date with the latest industry
+                    trends.
+                  </p>
+                </div>
+
+                <div class="feature">
+                  <i class="icon fas fa-cogs"></i>
+                  <h3>Customization</h3>
+                  <p>
+                    We understand that every business is unique. That’s why we
+                    create solutions tailored to your specific needs and goals.
+                  </p>
+                </div>
+
+                <div class="feature">
+                  <i class="icon fas fa-headset"></i>
+                  <h3>Customer-Centric Approach</h3>
+                  <p>
+                    We prioritize your satisfaction and provide top-notch
+                    support to address your IT concerns.
+                  </p>
+                </div>
+
+                <div class="feature">
+                  <i class="icon fas fa-money-bill-wave"></i>
+                  <h3>Affordability</h3>
+                  <p>
+                    We offer competitive pricing without compromising on the
+                    quality of our services.
+                  </p>
+                </div>
+
+                <div class="feature">
+                  <i class="icon fas fa-shield-alt"></i>
+                  <h3>Reliability</h3>
+                  <p>
+                    Count on us to be there when you need us. We are committed
+                    to ensuring your IT environment is reliable and available
+                    24/7.
+                  </p>
+                </div>
+              </section>
+
               <div className="btn btn-group">
                 <NavLink to="/contact">
                   <button className="button-container"> Connect Now</button>
